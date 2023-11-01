8065680b4632304eb3adc50f05bea13fdd84c2df
Ivan Ivic <ivanivic842@gmail.com>
2023-11-01 17:34:46 +0100

    contact page with dummy data added

---
+++ internals/handlers/contact.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+
+	"davidabram/go-templ-echo-htmx-template/internals/templates"
+
+	"github.com/donseba/go-htmx"
+	"github.com/labstack/echo/v4"
+)
+
+func (a *App) Contact(c echo.Context) error {
+	r := c.Request()
+	h := r.Context().Value(htmx.ContextRequestHeader).(htmx.HxRequestHeader)
+
+	b, _ := json.MarshalIndent(h, "", "\t")
+
+	page := &templates.Page{
+		Title: "Contact",
+		Boosted: h.HxBoosted,
+	}
+
+	components := templates.Contact(page, string(b))
+	return components.Render(context.Background(), c.Response().Writer)
+}
